8a331e1adec1bf94ca38afbf97041d08f9b7bc84
jackpot <ch.jackpot@gmail.com>
2023-03-21 13:10:54 +0900

    KakaoBlogSearchService 리팩토링

--- src/main/java/com/jackpot/surfing/api/service/KakaoBlogSearchService.java
+++ src/main/java/com/jackpot/surfing/api/service/KakaoBlogSearchService.java
@@ -3,8 +3,10 @@ package com.jackpot.surfing.api.service;
 import com.jackpot.surfing.api.domain.kakao.KakaoBlogSearchResponse;
 import com.jackpot.surfing.api.dto.BlogSearchCondition;
 import com.jackpot.surfing.api.dto.BlogSearchResultDto;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,49 +20,58 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RequiredArgsConstructor
 @Service
 public class KakaoBlogSearchService {
 
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String KAKAO_AK = "KakaoAK ";
     @Value("${kakao.api.blog.url}")
-    private String blogUrl;
+    private String BLOG_URL;
 
     @Value("${kakao.api.key}")
-    private String apiKey;
+    private String API_KEY;
 
     private final RestTemplate restTemplate;
 
     private final BlogSearchKeywordsService blogSearchKeywordsService;
 
     public Page<BlogSearchResultDto> searchBlogsPaging(BlogSearchCondition blogSearchCondition) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "KakaoAK " + apiKey);
+        ResponseEntity<KakaoBlogSearchResponse> kakaoBlogResponse = getKakaoBlogs(blogSearchCondition);
+        blogSearchKeywordsService.countSearchBlogKeyword(blogSearchCondition.getQuery());
 
-        UriComponents uriComponents = UriComponentsBuilder.fromHttpUrl(blogUrl)
+        List<BlogSearchResultDto> resultDtoList =
+            Objects.requireNonNull(kakaoBlogResponse.getBody()).getDocuments().stream()
+                .map(BlogSearchResultDto::convertFromKakao)
+                .collect(Collectors.toList());
+
+        return new PageImpl<>(resultDtoList, getPageable(blogSearchCondition), kakaoBlogResponse.getBody().getMeta().getPageable_count());
+    }
+
+    private static Pageable getPageable(BlogSearchCondition blogSearchCondition) {
+        Pageable pageable = PageRequest.of(blogSearchCondition.getPage() - 1, blogSearchCondition.getSize());
+        return pageable;
+    }
+
+    private ResponseEntity<KakaoBlogSearchResponse> getKakaoBlogs(BlogSearchCondition blogSearchCondition) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(AUTHORIZATION, KAKAO_AK + API_KEY);
+
+        return restTemplate.exchange(createUri(blogSearchCondition), HttpMethod.GET,
+            new HttpEntity<>(headers), KakaoBlogSearchResponse.class);
+    }
+
+    private URI createUri(BlogSearchCondition blogSearchCondition) {
+        return UriComponentsBuilder.fromHttpUrl(BLOG_URL)
             .queryParam("query", blogSearchCondition.getQuery())
             .queryParam("sort", blogSearchCondition.getSort())
             .queryParam("page", blogSearchCondition.getPage())
             .queryParam("size", blogSearchCondition.getSize())
             .encode(StandardCharsets.UTF_8)
-            .build();
-
-        HttpEntity<?> requestEntity = new HttpEntity<>(headers);
-
-        ResponseEntity<KakaoBlogSearchResponse> responseEntity =
-            restTemplate.exchange(uriComponents.toUri(), HttpMethod.GET, requestEntity, KakaoBlogSearchResponse.class);
-
-        blogSearchKeywordsService.countSearchBlogKeyword(blogSearchCondition.getQuery());
-
-        Pageable pageable = PageRequest.of(blogSearchCondition.getPage() - 1, blogSearchCondition.getSize());
-
-        List<BlogSearchResultDto> resultDtoList = responseEntity.getBody().getDocuments().stream()
-            .map(BlogSearchResultDto::convertFromKakao)
-            .collect(Collectors.toList());
-
-        return new PageImpl<>(resultDtoList, pageable, responseEntity.getBody().getMeta().getPageable_count());
+            .build()
+            .toUri();
     }
 }
 
